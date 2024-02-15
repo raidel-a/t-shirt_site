9d1078d8aa751436aa19a9af052a6e51de75be07
raihven <raihven@protonmail.com>
2024-02-15 00:31:10 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,8 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-// await import("./src/env.js");
-
-/** @type {import("next").NextConfig} */
-module.exports = {
-  experimental: {
-    images: {
-      unoptimized: true, // Enable image optimization
-    },
-  },
-  basePath: "/t-shirt_site",
-  reactStrictMode: true,
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  // i18n: {
-  //   locales: ["en"],
-  //   defaultLocale: "en",
-  // },
-};
-
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
